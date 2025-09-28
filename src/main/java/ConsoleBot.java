82bfa79cd386d11efb907da9bf55465d6dcbf9eb
Andrey <stolbov.andrey2006@gmail.com>
2025-09-28 20:52:12 +0500

    Добавил функцию echo

--- src/main/java/ConsoleBot.java
+++ src/main/java/ConsoleBot.java
@@ -1,12 +1,6 @@
-import java.util.Objects;
-import java.util.Scanner;
-
-
 public class ConsoleBot {
-    public static void main(String[] args){
-        LogicBot logicBot = new LogicBot();
-        logicBot.start();
-    }
-
-
+  public static void main(String[] args){
+    LogicBot logicBot = new LogicBot();
+    logicBot.start();
+  }
 }
--- src/main/java/Messages.java
+++ src/main/java/Messages.java
@@ -1,5 +1,5 @@
 public class Messages {
-    public static String firstMessage = "Привет!!! Это консольный бот, который пока ничего не делает. Напиши /help если тебе нужна будет помощь!!!!!! И /exit для выхода.";
-    public static String helpMessage = "Помощи нет:(";
-
+  public static String firstMessage = "Привет!!! Это консольный бот. Напиши /help если нужна помощь и /exit для выхода.";
+  public static String helpMessage = "Доступные команды: /help - помощь, /exit - выход. Всё остальное я повторяю.";
 }
+
