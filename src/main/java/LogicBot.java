f34b9f4cf70b5a057f405b4ace9fb570356e6683
Andrey <stolbov.andrey2006@gmail.com>
2025-09-28 20:52:12 +0500

    Сделал отдельный класс с функцией эхо

--- src/main/java/LogicBot.java
+++ src/main/java/LogicBot.java
@@ -1,38 +1,34 @@
 import java.util.Scanner;
 
 public class LogicBot {
-  public void start(){
-    System.out.println(Messages.firstMessage);
-    Scanner in = new Scanner(System.in);
-    while(true){
-      String message = in.nextLine();
-      switch(message){
-        case "/help":{
-          help();
-          break;
+    public void start() {
+        System.out.println(Messages.firstMessage);
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            String message = in.nextLine();
+            switch (message) {
+                case "/help": {
+                    help();
+                    break;
+                }
+                case "/exit": {
+                    exit();
+                    break;
+                }
+                case "/echo": {
+                    Echo.echo();
+                    break;
+                }
+            }
         }
-        case "/exit":{
-          exit();
-          break;
-        }
-        default:{
-          echo(message);
-          break;
-        }
-      }
     }
-  }
 
-  public static void help(){
-    System.out.println(Messages.helpMessage);
-  }
+    public static void help() {
+        System.out.println(Messages.helpMessage);
+    }
 
-  public static void exit(){
-    System.out.println("До свидания!");
-    System.exit(0);
-  }
-
-  public static void echo(String message){
-    System.out.println("Вы сказали: " + message);
-  }
+    public static void exit() {
+        System.out.println("До свидания!");
+        System.exit(0);
+    }
 }
--- src/main/java/Messages.java
+++ src/main/java/Messages.java
@@ -1,5 +1,6 @@
 public class Messages {
-  public static String firstMessage = "Привет!!! Это консольный бот. Напиши /help если нужна помощь и /exit для выхода.";
-  public static String helpMessage = "Доступные команды: /help - помощь, /exit - выход. Всё остальное я повторяю.";
+  public static String firstMessage = "Привет!!! Это консольный бот. Напиши /help если нужна помощь, /echo - функция Эхо(повторяет введенное сообщение), /exit для выхода.";
+  public static String helpMessage = "Доступные команды: /help - помощь, /exit - выход, /echo - повторяю введенное сообщение.";
+  public static String echoMessage = "Это функция эхо, напишите что-нибудь, я буду за вами повторять.Доступные команды: /help - помощь, /exit - для выхода, /back - вернуться назад";
 }
 
