9b4c14622f516dac6b05896ce18c555a0173625c
artemmedvedev31og <artemw2017@gmai.com>
2025-11-25 19:23:19 +0500

    Связал бота с телеграммом, прописал логику, создал класс для запроса на сайт Genius и класс с парсером

---
+++ src/main/java/Keys.java
@@ -0,0 +1,29 @@
+public class Keys {
+    public String getBotToken() {
+        return botToken;
+    }
+
+    public void setBotToken(String botToken) {
+        this.botToken = botToken;
+    }
+
+    public String getGeniusApiToken() {
+        return geniusApiToken;
+    }
+
+    public void setGeniusApiToken(String geniusApiToken) {
+        this.geniusApiToken = geniusApiToken;
+    }
+
+    public String getBotUsername() {
+        return botUsername;
+    }
+
+    public void setBotUsername(String botUsername) {
+        this.botUsername = botUsername;
+    }
+
+    private String botToken;
+    private String botUsername;
+    private String geniusApiToken;
+}
